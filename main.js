0fb3deec84f8e6342a6480766199feb802eb8e8c
Firee <newowner@MacBookPro.attlocal.net>
2026-04-10 20:57:42 -0400

    Restructure project and all source files

--- main.js
+++
@@ -1,6 +0,0 @@
-const darkModebtn = document.getElementById('dark-mode-btn')
-const currenTheme = localStorage('theme')
-
-if( currenTheme){
-    document.documentElement.setAttribute('data-theme', currenTheme)
-}
